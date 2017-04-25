5474a37043b8e2920f8edaa94b3b5b080666b030
Bradley Saxman <BradSaxman@Bradleys-MacBook-Pro.local>
2017-04-25 14:15:18 -0400

    2nd test passing

--- cypher.py
+++ cypher.py
@@ -1,3 +1,6 @@
+
+	# normalizes that input text
+
 def normalize(text):
 	
 	text = text.lower()
@@ -7,3 +10,18 @@ def normalize(text):
 	text = re.sub(r'\W+', '',text)
 	
 	return text
+
+
+	# splits the normalized text into rows that make up the necessary 'square'
+
+def square(text):
+	import math
+	rows = []
+	for i in range( 0,len(text),int(math.ceil(math.sqrt(len(text)))) ):
+		rows.append( text[i:i+int(math.ceil(math.sqrt(len(text))))] )
+	return rows
+
+
+def encrypt(rows):
+
+	return newRows
--- test_cypher.py
+++ test_cypher.py
@@ -1,4 +1,13 @@
 from cypher import normalize
+from cypher import square
+from cypher import encrypt
 
-def test_answer():
+
+def test_normalize():
 	assert normalize("Text that must be normalized!") == "textthatmustbenormalized"
+
+def test_square():
+	assert square("textthatmustbenormalized") == ["textt","hatmu","stben","ormal","ized"]
+
+def test_encrypt():
+	assert encrypt(["textt","hatmu","stben","ormal","ized"]) == ["thsoi","eatrz","xtbme","tmead","tunl"]
