cb9e44128f79f963d730f9c47c4f28d859de32a4
Bradley Saxman <BradSaxman@Bradleys-MacBook-Pro.local>
2017-04-26 14:16:58 -0400

    full code, all tests passing

--- cypher.py
+++ cypher.py
@@ -1,5 +1,5 @@
 
-	# normalizes that input text
+	# normalizes the input text
 
 def normalize(text):
 	
@@ -11,7 +11,6 @@ def normalize(text):
 	
 	return text
 
-
 	# splits the normalized text into rows that make up the necessary 'square'
 
 def square(text):
@@ -21,7 +20,28 @@ def square(text):
 		rows.append( text[i:i+int(math.ceil(math.sqrt(len(text))))] )
 	return rows
 
+	# pads the rows with temporary puncuation to avoid
+	# out of index errors in the encrypt function
+
+def pad(rows):
+	while len(rows[0]) != len(rows[-1]):
+		rows[-1] += '!'
+	return rows
+	
+	# reorders the squared rows into the encrypted rows
 
 def encrypt(rows):
+	output = []
+	for i in range(len(rows[0])):
+		word = []
+		for j in range(len(rows)):
+			word += rows[j][i]
+		output += word
+	return ''.join(output)
 
-	return newRows
+	# condenses all of the above functions into one function 
+	# to fully encrypt the input message
+
+def encryptComplete(text):
+	message = normalize(encrypt(pad(square(normalize(text)))))
+	return message
--- test_cypher.py
+++ test_cypher.py
@@ -1,13 +1,31 @@
 from cypher import normalize
 from cypher import square
+from cypher import pad
 from cypher import encrypt
+from cypher import encryptComplete
 
 
 def test_normalize():
 	assert normalize("Text that must be normalized!") == "textthatmustbenormalized"
+	assert normalize("Texts that must be normalized!") == "textsthatmustbenormalized"
+	assert normalize("Is this code going to work? Let's see!") == "isthiscodegoingtoworkletssee"
 
 def test_square():
 	assert square("textthatmustbenormalized") == ["textt","hatmu","stben","ormal","ized"]
+	assert square("textsthatmustbenormalized") == ["texts","thatm","ustbe","norma","lized"]
+	assert square("isthiscodegoingtoworkletssee") == ['isthis','codego','ingtow','orklet','ssee']
+
+def test_pad():
+	assert pad(["textt","hatmu","stben","ormal","ized"]) == ["textt","hatmu","stben","ormal","ized!"]
+	assert pad(["texts","thatm","ustbe","norma","lized"]) == ["texts","thatm","ustbe","norma","lized"]
+	assert pad(['isthis','codego','ingtow','orklet','ssee']) == ['isthis','codego','ingtow','orklet','ssee!!']
 
 def test_encrypt():
-	assert encrypt(["textt","hatmu","stben","ormal","ized"]) == ["thsoi","eatrz","xtbme","tmead","tunl"]
+	assert encrypt(["textt","hatmu","stben","ormal","ized!"]) == "thsoieatrzxtbmetmeadtunl!"
+	assert encrypt(["texts","thatm","ustbe","norma","lized"]) == "ttunlehsoixatrzttbmesmead"
+	assert encrypt(['isthis','codego','ingtow','orklet','ssee!!']) == "iciossonrstdgkehetleigoe!sowt!"
+
+def test_encryptComplete():
+	assert encryptComplete("Text that must be normalized!") == "thsoieatrzxtbmetmeadtunl"
+	assert encryptComplete("Texts that must be normalized!") == "ttunlehsoixatrzttbmesmead"
+	assert encryptComplete("Is this code going to work? Let's see!") == "iciossonrstdgkehetleigoesowt"
